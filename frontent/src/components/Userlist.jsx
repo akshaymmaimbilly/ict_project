08da36ecf4995e58da6eb0bbb0c9c53edb03f6f6
Mohammadsiddiqueps <siddiqueps333@gmail.com>
2023-06-04 21:39:04 +0530

    backend

--- frontent/src/components/Userlist.jsx
+++ frontent/src/components/Userlist.jsx
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { DataGrid } from '@mui/x-data-grid';
+import Navbar from '../comp/navbar/Navbar';
+import Sidebar from '../comp/sidebar/Sidebar';
+import './userlist.scss';
+// import { fontGrid } from '@mui/material/styles/cssUtils';
 
 const Userlist = () => {
   const [users, setUsers] = useState([]);
   const [editingUserId, setEditingUserId] = useState('');
   const [editedUser, setEditedUser] = useState({});
-
+  
+  // export { users };
   useEffect(() => {
     fetchUsers();
   }, []);
-
+  
+  // const userCount = users.length;
+  // export { userCount };
+  // module.exports = {
+  //   userCount
+  // };
   const fetchUsers = async () => {
     try {
       const response = await axios.get('http://localhost:8080/viewusers');
@@ -18,12 +29,12 @@ const Userlist = () => {
       console.error('Error fetching users:', error);
     }
   };
-
+  
   const handleEdit = (user) => {
     setEditingUserId(user._id);
     setEditedUser(user);
   };
-
+  
   const handleSave = async () => {
     try {
       await axios.put(`http://localhost:8080/users/${editingUserId}`, editedUser);
@@ -43,75 +54,114 @@ const Userlist = () => {
   const handleInputChange = (e) => {
     setEditedUser({ ...editedUser, [e.target.name]: e.target.value });
   };
-
+  
   const handleDelete = async (id) => {
     try {
       await axios.delete(`http://localhost:8080/users/${id}`);
       fetchUsers();
+      alert("User Deleted successfully")
     } catch (error) {
       console.error('Error deleting user:', error);
     }
   };
+  
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 100 },
+    { field: 'name', headerName: 'Name', width: 250 },
+    { field: 'email', headerName: 'Email', width: 350 },
+    { field: 'age', headerName: 'Age', width: 150 },
+
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 150,
+      renderCell: (params) => (
+        <div>
+          {params.row.editing ? (
+            <>
+              <button onClick={handleSave}>Save</button>
+              <button onClick={handleCancel}>Cancel</button>
+            </>
+          ) : (
+            <>
+              <button onClick={() => handleEdit(params.row)} className='viewButton'>Edit</button>
+              <button onClick={() => handleDelete(params.row.id)} className='deleteButton'>Delete</button>
+            </>
+          )}
+        </div>
+      ),
+    }
+  ];
+  
+  const rows = users.map((user) => ({
+    id: user._id,
+    name: editingUserId === user._id ? (
+      <input
+        type="text"
+        name="name"
+        value={users.username
+          || ''}
+        onChange={handleInputChange}
+      />
+    ) : (
+      user.username
+
+    ),
+    // age: editingUserId === user._id ? (
+    //   <input
+    //     type="number"
+    //     name="age"
+    //     value={editedUser.age|| ''}
+    //     onChange={handleInputChange}
+    //   />
+    // ) : (
+    //   user.age
+
+    // ),
+    email: editingUserId === user._id ? (
+      <input
+        type="email"
+        name="email"
+        value={editedUser.email || ''}
+        onChange={handleInputChange}
+      />
+    ) : (
+      user.email
+    ),
+    editing: editingUserId === user._id,
+  }));
 
   return (
+   
     <div>
-      <h1>User List</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user._id}>
-              <td>{user._id}</td>
-              <td>
-                {editingUserId === user._id ? (
-                  <input
-                    type="text"
-                    name="name"
-                    value={editedUser.name || ''}
-                    onChange={handleInputChange}
-                  />
-                ) : (
-                  user.name
-                )}
-              </td>
-              <td>
-                {editingUserId === user._id ? (
-                  <input
-                    type="email"
-                    name="email"
-                    value={editedUser.email || ''}
-                    onChange={handleInputChange}
-                  />
-                ) : (
-                  user.email
-                )}
-              </td>
-              <td>
-                {editingUserId === user._id ? (
-                  <>
-                    <button onClick={handleSave}>Save</button>
-                    <button onClick={handleCancel}>Cancel</button>
-                  </>
-                ) : (
-                  <>
-                    <button onClick={() => handleEdit(user)}>Edit</button>
-                    <button onClick={() => handleDelete(user._id)}>Delete</button>
-                  </>
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      
+      <div className="bars" style={{ display: 'flex' }}>
+        <div className="side" style={{ justifyContent: 'space-between' }}>
+          <Sidebar />
+        </div>
+        <div className="content">
+          <Navbar />
+          <div className="users" style={{padding:'25px'}}>
+            <h1 style={{backgroundColor:'#c7eddb', textAlign:'center', height:'50px',fontSize:'35px',color:'#325c49'}}>User List</h1>
+            <div className='darking' style={{ height: 400, width: '100%' }}>
+              <DataGrid
+              
+            style={{ fontGrid:'20px',fontSize:'20px'}}
+              className="datagrid"
+              rows={rows} 
+              columns={columns} 
+              pageSize={5} 
+              checkboxSelection
+              ClassName="darker"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
+// export users;
+// export { userCount };
 
 export default Userlist;
